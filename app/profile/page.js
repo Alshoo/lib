836b34b0ffd24e17935373f07a25139760759902
Alshoo <108189737+Alshoo@users.noreply.github.com>
2024-11-08 18:51:54 +0200

    7

--- app/profile/page.js
+++ app/profile/page.js
@@ -1,17 +1,19 @@
-import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/route";
+// import { getServerSession } from "next-auth";
+// import { authOptions } from "../api/auth/[...nextauth]/route";
 
 export default async function ProfilePage() {
-  const session = await getServerSession(authOptions);
+  // const session = await getServerSession(authOptions);
 
-  if (!session) {
+  // if (!session) {
 
-    return <p>Redirecting to login...</p>;
-  }
+  //   return <p>Redirecting to login...</p>;
+  // }
 
   return (
     <div>
-      <h1>Welcome, {session.user.email}</h1>
+      <h1>Welcome,
+         {/* {session.user.email} */}
+         </h1>
     </div>
   );
 }
