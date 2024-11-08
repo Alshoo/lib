943f779235017211e453d74f07920c21e80d4516
Alshoo <108189737+Alshoo@users.noreply.github.com>
2024-11-08 19:00:07 +0200

    10

--- pages/Home/ratingStar.js
+++ pages/Home/ratingStar.js
@@ -1,4 +1,6 @@
-const RatingStars = ({ rating }) => {
+
+export default function RatingStars({ rating }) {
+
   // const fullStars = Math.max(0, Math.floor(rating));
   // const halfStar = rating % 1 !== 0;
   // const emptyStars = Math.max(0, 5 - fullStars - (halfStar ? 1 : 0));
@@ -8,8 +10,8 @@ const RatingStars = ({ rating }) => {
       {/* {Array(fullStars).fill(<span className="star full">★</span>)}
       {halfStar && <span className="star half">★</span>}
       {Array(emptyStars).fill(<span className="star empty">★</span>)} */}
+
+      {rating}
     </div>
   );
 };
-
-export default RatingStars;
