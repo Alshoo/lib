9916a82ebc4f404fa8fb83f7fc354fb05a1a8c69
Alshoo <108189737+Alshoo@users.noreply.github.com>
2024-11-08 19:04:59 +0200

    11

--- app/profile/page.js
+++ app/profile/page.js
@@ -1,7 +1,7 @@
 // import { getServerSession } from "next-auth";
 // import { authOptions } from "../api/auth/[...nextauth]/route";
 
-export default async function ProfilePage() {
+export default function ProfilePage() {
   // const session = await getServerSession(authOptions);
 
   // if (!session) {
