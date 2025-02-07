c54e4bd363ad0b64392e16d46dcf4d8313d6ad7e
Alshoo <108189737+Alshoo@users.noreply.github.com>
2025-02-07 18:33:26 +0200

    ,,

--- pages/Home/Home.js
+++ pages/Home/Home.js
@@ -38,8 +38,6 @@ export default function Home() {
           <div className="headLineText">
             <h3>مرحبا بك فى قارئ</h3>
             <h4>استمتع بقراءة الكتب المفضلة لديك في أي وقت وأي مكان</h4>
-            <h4>استمتع بقراءة الكتب المفضلة لديك في أي وقت وأي مكان</h4>
-            <h4>استمتع بقراءة الكتب المفضلة لديك في أي وقت وأي مكان</h4>
           </div>
 
             <div className="searchContainer">
